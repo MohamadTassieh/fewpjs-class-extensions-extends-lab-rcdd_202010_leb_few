830c227866b46dc5bd89a1a1ed7b49f44f233269
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 15:08:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ class Polygon {
      return p;
    }
 }
+
+class Triangle extends Polygon{
+  get isValid(){
+    if(this.array[0]<this.array[1]+this.array[2]){
+      return true;
+   }
+   else{
+      return false;
+   }
+  }
+}
