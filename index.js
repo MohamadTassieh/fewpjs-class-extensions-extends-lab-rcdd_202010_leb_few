a31a17eb235cf2b18b344c795b97f80b43a39e08
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 15:12:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ class Triangle extends Polygon{
     get isValid(){
   if(this.array[0]===this.array[1] && this.array[0]===this.array[2] && this.array[0]===this.array[3]){
     return true;
-    }else{
+    }
+    else{
     return false;
     }
     }
