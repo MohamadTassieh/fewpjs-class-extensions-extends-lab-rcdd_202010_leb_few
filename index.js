ea3c65833aae4f4bab33353ef58d11f4df415669
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 15:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ class Polygon {
   get countSides(){
      return this.array.length;
   }
+  get perimeter(){
+    let p = 0;
+     for(let i=0;i<this.array.length;i++){
+       p += this.array[i];
+     }
+     return p;
+   }
 }
