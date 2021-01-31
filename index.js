4a8420a06053accaffa5376995d66620498dab71
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 15:13:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ class Square extends Polygon{
   get isValid(){
 if(this.array[0]===this.array[1] && this.array[0]===this.array[2] && this.array[0]===this.array[3]){
   return true;
-  }else{
+  }
+  else{
   return false;
   }
   }
